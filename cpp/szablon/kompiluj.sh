4f5bae03bcc28ed9b8cdc7f2d963bb46140ab8b6
FilipFilipczak <filip.filipczak.e-zstih.edu.pl>
2024-02-22 13:36:20 +0000

    studentmachine send

--- cpp/szablon/kompiluj.sh
+++ cpp/szablon/kompiluj.sh
@@ -4,4 +4,6 @@ echo "----Kompilacja programu----"
 g++ -o main main.cpp
 echo "---Kompilacja ukonczona----"
 echo "---Uruchamianie programu---"
+chmod +x main
 ./main
+echo "---Zakonczono program---"
