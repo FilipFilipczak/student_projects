486877ef94253df12718f7d0b8ac1f538afd5f33
FilipFilipczak <filip.filipczak@e-zstih.edu.pl>
2024-04-11 13:05:11 +0000

    studentmachine send

---
+++ python/cwiczenie/main.py
@@ -0,0 +1,13 @@
+print ("witaj swiecie\n")
+x=float(input("Podaj x\n"))
+print (x)
+y=float(input("Podaj y\n"))
+print (x+y)
+if x>10:
+	print ("x>10 11")
+elif x==10 and y==20:
+	print ("x=10 i y=20")
+else:
+	print("x<10 9")
+for z in range(0,10):
+	print (z)
